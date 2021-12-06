3d212b2ba730df6383277eeeaf299691413cd248
Fabien PETITJEAN <fabien.petitjean@epfl.ch>
2021-12-06 16:14:39 +0100

    Remove SASS

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,40 @@
-const package = require("./package.json")
+const package = require('./package.json')
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CopyPlugin = require('copy-webpack-plugin')
-const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
     cache: {
-        type: "memory",
+        type: 'memory',
     },
     output: {
-        filename: "scr/[name].[contenthash].js",
-        path: path.resolve(__dirname, "build"),
+        filename: 'scr/[name].[contenthash].js',
+        path: path.resolve(__dirname, 'build'),
     },
     entry: {
-        app: "./src/index.tsx",
+        app: './src/index.tsx',
     },
-    target: "web",
+    target: 'web',
     resolve: {
-        extensions: [".tsx", ".ts", ".js", ".jsx", ".wasm"],
+        extensions: ['.tsx', '.ts', '.js', '.jsx', '.wasm'],
         enforceExtension: false,
         alias: {
-            "@/contract": path.resolve(__dirname, "src/contract"),
-            "@/mock": path.resolve(__dirname, "src/mock"),
-            "@/tool": path.resolve(__dirname, "src/tool"),
-            "@/ui": path.resolve(__dirname, "src/ui"),
-            "@/view": path.resolve(__dirname, "src/view"),
+            '@/contract': path.resolve(__dirname, 'src/contract'),
+            '@/mock': path.resolve(__dirname, 'src/mock'),
+            '@/tool': path.resolve(__dirname, 'src/tool'),
+            '@/ui': path.resolve(__dirname, 'src/ui'),
+            '@/view': path.resolve(__dirname, 'src/view'),
         },
     },
-    devtool: "inline-source-map",
+    devtool: 'inline-source-map',
     devServer: {
         compress: true,
         static: {
-            directory: path.resolve(__dirname, "./public"),
+            directory: path.resolve(__dirname, './public'),
         },
         client: {
-            logging: "verbose",
+            logging: 'verbose',
             overlay: { errors: true, warnings: false },
             progress: true,
         },
@@ -52,35 +52,35 @@ module.exports = {
         new CopyPlugin({
             patterns: [
                 {
-                    from: path.resolve(__dirname, "public"),
-                    filter: async path => {
-                        return !path.endsWith("index.html")
+                    from: path.resolve(__dirname, 'public'),
+                    filter: async (path) => {
+                        return !path.endsWith('index.html')
                     },
                 },
             ],
         }),
         new HtmlWebpackPlugin({
-            template: "public/index.html",
-            filename: "index.html",
+            template: 'public/index.html',
+            filename: 'index.html',
             version: package.version,
-            title: "Brayns Viewer",
+            title: 'Brayns Viewer',
         }),
     ],
     optimization: {
         // Create a single runtime bundle for all chunks.
-        runtimeChunk: "single",
+        runtimeChunk: 'single',
         splitChunks: {
             // All the node_modules libs on a single file.
             cacheGroups: {
                 vendor: {
                     test: /[\\/]node_modules[\\/]/,
-                    name: "libs",
-                    chunks: "all",
+                    name: 'libs',
+                    chunks: 'all',
                 },
             },
         },
         // Prevent "libs.[contenthash].js" from changing its hash if not needed.
-        moduleIds: "deterministic",
+        moduleIds: 'deterministic',
     },
     module: {
         rules: [
@@ -88,7 +88,7 @@ module.exports = {
                 test: /\.tsx?$/,
                 use: [
                     {
-                        loader: "ts-loader",
+                        loader: 'ts-loader',
                         options: {
                             transpileOnly: false,
                         },
@@ -98,40 +98,34 @@ module.exports = {
             },
             {
                 test: /\.(png|svg|jpe?g|gif|webp)$/,
-                loader: "url-loader",
+                loader: 'url-loader',
                 options: {
                     limit: 8192,
-                    name: "img/[name].[contenthash].[ext]",
+                    name: 'img/[name].[contenthash].[ext]',
                 },
             },
             {
                 test: /\.(woff|woff2|eot|ttf|otf)$/,
-                loader: "file-loader",
+                loader: 'file-loader',
                 options: {
-                    name: "fnt/[contenthash].[ext]",
+                    name: 'fnt/[contenthash].[ext]',
                 },
             },
             {
                 test: /\.(frag|vert)$/,
-                loader: "raw-loader",
+                loader: 'raw-loader',
             },
             {
                 test: /\.ya?ml$/,
-                type: "json",
-                use: "yaml-loader",
+                type: 'json',
+                use: 'yaml-loader',
             },
             {
                 test: /\.(scss|css)$/,
                 use: [
-                    "style-loader",
+                    'style-loader',
                     {
-                        loader: "css-loader",
-                        options: {
-                            sourceMap: true,
-                        },
-                    },
-                    {
-                        loader: "sass-loader",
+                        loader: 'css-loader',
                         options: {
                             sourceMap: true,
                         },
